c81db30f0d700b96c85839ec0065a89f565affe9
Alex Elias <alex.elias@edu.pucrs.br>
2022-03-08 17:52:48 -0300

    add create user

--- src/addUser/handler.js
+++ src/addUser/handler.js
@@ -1,11 +1,15 @@
 const User = require("../../models/user.model");
-
 const mongoose = require("mongoose");
-const mongooseOptions = {
-  useNewUrlParse:true,
-  useCreateIndex:true,
-};
 
-exports.handler = async(event,context,callback) =>{
- 
+module.exports.addUser = async (event, context, callback) => {
+  const uri = process.env.URL_MONGO;
+  await mongoose.connect(uri);
+
+  const user = await User.create({
+    user: event.body.user,
+    password: event.body.password,
+    role: event.body.role
+  })
+
+  return user;
 }
